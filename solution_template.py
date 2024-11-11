7628700809965bc67b3cfd6a452966c9bff61720
ryan <ryan.h153@gmail.com>
2024-11-10 21:43:08 -0700

    Timer in template

--- solution_template.py
+++ solution_template.py
@@ -1,3 +1,6 @@
+from time import perf_counter
+
+
 def part_one(file: str):
     pass
 
@@ -8,6 +11,7 @@ def part_two(file: str):
 
 def main() -> None:
     input_type = 'sample'
+    start = perf_counter()
     print(f'First: {part_one(f'first_{input_type}_input.txt')}')
     print(f'Second: {part_two(f'first_{input_type}_input.txt')}')
     print(f'Run time {(perf_counter() - start) * 1e3:,.2} ms')
